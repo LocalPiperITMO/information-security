be1dc0b53238c0fa86d6af8ee5dee78542865025
Local Piper <artsor53@gmail.com>
2025-09-13 12:35:11 +0300

    chore: project template

---
+++ iblab1/src/main/java/itmo/localpiper/iblab1/Iblab1Application.java
@@ -0,0 +1,13 @@
+package itmo.localpiper.iblab1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iblab1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Iblab1Application.class, args);
+	}
+
+}
---
+++ iblab1/src/test/java/itmo/localpiper/iblab1/Iblab1ApplicationTests.java
@@ -0,0 +1,13 @@
+package itmo.localpiper.iblab1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iblab1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
